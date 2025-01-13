cefbd528d43cdd7a409a298eb04e6c406e69abf6
daniels9028 <daniels9028@student.untan.ac.id>
2025-01-13 14:46:25 +0700

    Add home, login, register and not found route for initial routes

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import Login from "./Authentication/Login";
+import Register from "./Authentication/Register";
+import Page404 from "./ErrorPage/Page404";
+import Home from "./Home/Home";
+
+export { Login, Register, Page404, Home };
