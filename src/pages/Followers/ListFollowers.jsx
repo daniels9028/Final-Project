48eb6afb27e96952e38ecf695cab63b066f43639
daniels9028 <daniels9028@student.untan.ac.id>
2025-01-13 11:58:54 +0700

    feat: First Initiate with react js, tailwind css and react router dom

---
+++ src/pages/Followers/ListFollowers.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ListFollowers = () => {
+  return <div>ListFollowers</div>;
+};
+
+export default ListFollowers;
---
+++ src/pages/Following/ListFollowing.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ListFollowing = () => {
+  return <div>ListFollowing</div>;
+};
+
+export default ListFollowing;
