e1807f90447db733b0a6c48d0f6207bd2e6c020f
daniels9028 <daniels9028@student.untan.ac.id>
2025-01-14 08:49:15 +0700

    feat: Add posts services like create post,update post, delete post, get explore post, get posts by user id, get post by id and get my following post

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useAuth } from "../../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 import { Logout } from "../../services/Authentication";
+import {
+  createPost,
+  deletePost,
+  getExplorePost,
+  getMyFollowingPost,
+  getPostById,
+  getPostByUserId,
+  updatePost,
+} from "../../services/Post";
 
 const Home = () => {
   const { token } = useAuth();
   const navigate = useNavigate();
+
+  const [file, setFile] = useState(null);
+
+  const [form, setForm] = useState({
+    caption: "Pengen ayam",
+  });
+
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+  };
 
   const logout = async () => {
     try {
@@ -20,9 +39,97 @@ const Home = () => {
       console.log(error);
     }
   };
+
+  const explorePost = async () => {
+    try {
+      const data = await getExplorePost({ size: 10, page: 1 });
+
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const postByUserId = async () => {
+    try {
+      const data = await getPostByUserId(
+        { size: 10, page: 1 },
+        "5b7a6783-2071-4e9f-9b8e-8e7fc4a981d4"
+      );
+
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const postById = async () => {
+    try {
+      const data = await getPostById("7c8a3a30-7191-4b6c-bcd9-fef3468801ae");
+
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const followingPost = async () => {
+    try {
+      const data = await getMyFollowingPost({ size: 10, page: 1 });
+
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const create = async () => {
+    try {
+      const data = await createPost({ ...form, file });
+
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const update = async () => {
+    try {
+      const data = await updatePost(
+        { ...form, file },
+        "763d2432-12fa-43e5-960e-7d0640d095a1"
+      );
+
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const destroyPost = async () => {
+    try {
+      const data = await deletePost("763d2432-12fa-43e5-960e-7d0640d095a1");
+
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    // explorePost();
+    // postByUserId();
+    // postById();
+    // followingPost();
+  }, []);
+
   return (
     <div>
-      <button onClick={logout}>logout</button>
+      <button onClick={logout}>logout</button> <br />
+      <input type="file" onChange={handleFileChange} required />
+      <button onClick={create}>Create</button>
+      <button onClick={update}>Update</button>
+      <button onClick={destroyPost}>Delete</button>
     </div>
   );
 };
--- src/services/Post.jsx
+++ src/services/Post.jsx
@@ -1,7 +1,126 @@
-export const createPost = () => {};
-export const updatePost = () => {};
-export const deletePost = () => {};
-export const getExplorePost = () => {};
-export const getPostByUserId = () => {};
-export const getPostByPostId = () => {};
-export const getMyFollowingPost = () => {};
+import { axiosInstance as axios } from "../axios/axios";
+import { uploadImage } from "./Upload";
+
+const token = localStorage.getItem("token");
+
+export const createPost = async (dataPost) => {
+  try {
+    const { url } = await uploadImage(dataPost.file);
+
+    const { file, ...data } = dataPost;
+
+    const create = await axios({
+      url: "api/v1/create-post",
+      method: "POST",
+      data: { ...data, imageUrl: url },
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    return create.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
+
+export const updatePost = async (dataPost, postId) => {
+  try {
+    const { url } = await uploadImage(dataPost.file);
+
+    const { file, ...data } = dataPost;
+
+    const update = await axios({
+      url: `api/v1/update-post/${postId}`,
+      method: "POST",
+      data: { ...data, imageUrl: url },
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    return update.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
+
+export const deletePost = async (postId) => {
+  try {
+    const request = await axios({
+      url: `api/v1/delete-post/${postId}`,
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    return request.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
+
+export const getExplorePost = async (request) => {
+  try {
+    const explore = await axios({
+      url: `api/v1/explore-post?size=${request.size}&page=${request.page}`,
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    return explore.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
+
+export const getPostByUserId = async (request, userId) => {
+  try {
+    const userPost = await axios({
+      url: `api/v1/users-post/${userId}?size=${request.size}&page=${request.page}`,
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    return userPost.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
+
+export const getPostById = async (postId) => {
+  try {
+    const postById = await axios({
+      url: `api/v1/post/${postId}`,
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    return postById.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
+
+export const getMyFollowingPost = async (request) => {
+  try {
+    const followingPost = await axios({
+      url: `api/v1/following-post?size=${request.size}&page=${request.page}`,
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    return followingPost.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
