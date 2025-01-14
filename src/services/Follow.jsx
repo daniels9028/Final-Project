0e6e8fc04cd75707cd88148fd052d0d46754486e
daniels9028 <daniels9028@student.untan.ac.id>
2025-01-14 09:18:31 +0700

    feat: Add follow services follow user, unfollow user, get my following, get my followers, get following by user id and get followers by user id

--- src/services/Follow.jsx
+++ src/services/Follow.jsx
@@ -1,6 +1,101 @@
-export const followUser = () => {};
-export const unfollowUser = () => {};
-export const getMyFollowing = () => {};
-export const getMyFollowers = () => {};
-export const getFollowingByUserId = () => {};
-export const getFollowersByUserId = () => {};
+import { axiosInstance as axios } from "../axios/axios";
+
+const token = localStorage.getItem("token");
+
+export const followUser = async (data) => {
+  try {
+    console.log(data);
+    const follow = await axios({
+      url: `api/v1/follow`,
+      method: "POST",
+      data: data,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    return follow.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
+
+export const unfollowUser = async (userId) => {
+  try {
+    const unfollow = await axios({
+      url: `api/v1/unfollow/${userId}`,
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    return unfollow.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
+
+export const getMyFollowing = async (request) => {
+  try {
+    const myFollowing = await axios({
+      url: `api/v1/my-following?size=${request.size}&page=${request.page}`,
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    return myFollowing.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
+
+export const getMyFollowers = async (request) => {
+  try {
+    const myFollowers = await axios({
+      url: `api/v1/my-followers?size=${request.size}&page=${request.page}`,
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    return myFollowers.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
+
+export const getFollowingByUserId = async (request, userId) => {
+  try {
+    const followingByUserId = await axios({
+      url: `api/v1/following/${userId}?size=${request.size}&page=${request.page}`,
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    return followingByUserId.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
+
+export const getFollowersByUserId = async (request, userId) => {
+  try {
+    const followersByUserId = await axios({
+      url: `api/v1/followers/${userId}?size=${request.size}&page=${request.page}`,
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    return followersByUserId.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
