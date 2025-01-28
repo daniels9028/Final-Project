e6dd405c7c4eb264b5de32a427ef4d02da155d5c
daniels9028 <daniels9028@student.untan.ac.id>
2025-01-28 10:14:17 +0700

    feat: Fixed profile page with redesign in mobile and website

---
+++ src/components/ListMyPost.jsx
@@ -0,0 +1,73 @@
+import React, { useEffect } from "react";
+import { alternativeImageUrlPost } from "../assets";
+import DetailPost from "./DetailPost";
+import { useComment, useNavigateUser, usePostById } from "../hooks";
+import { useAuth } from "../context/AuthContext";
+
+const ListMyPost = ({ post }) => {
+  const explore = post;
+
+  const { handleNavigate } = useNavigateUser();
+
+  const { auth } = useAuth();
+
+  const { user } = auth;
+
+  const {
+    handleGetPostById,
+    totalComment,
+    isModalPostOpen,
+    openModalPost,
+    closeModalPost,
+    comments,
+  } = usePostById();
+
+  const {
+    handleAddComment,
+    handleDeleteComment,
+    form,
+    setForm,
+    submitComment,
+  } = useComment();
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      handleAddComment(explore?.id);
+    }
+  };
+
+  useEffect(() => {
+    handleGetPostById(explore?.id);
+  }, [submitComment]);
+
+  return (
+    <>
+      <img
+        src={post.imageUrl || alternativeImageUrlPost}
+        alt={post.id}
+        className="object-cover transition-all rounded-lg shadow-lg cursor-pointer hover:opacity-70"
+        onError={(e) => {
+          e.target.src = alternativeImageUrlPost;
+        }}
+        onClick={openModalPost}
+      />
+
+      <DetailPost
+        isOpen={isModalPostOpen}
+        onClose={closeModalPost}
+        explore={explore}
+        comments={comments}
+        auth={auth}
+        totalComment={totalComment}
+        form={form}
+        setForm={setForm}
+        handleKeyDown={handleKeyDown}
+        handleAddComment={handleAddComment}
+        handleDeleteComment={handleDeleteComment}
+        handleNavigate={handleNavigate}
+      />
+    </>
+  );
+};
+
+export default ListMyPost;
